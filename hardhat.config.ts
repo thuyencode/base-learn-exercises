2cce34ea730dcf0968aa6f12521291627ae7f80e
Thuyen Code <thuyencode@proton.me>
2025-02-18 22:12:45 +0700

    Change compiler version to avoid some error related to the previous version, according to Etherscan
    "Compiler specific version warnings:
    
    The compiled contract might be susceptible to VerbatimInvalidDeduplication (low-severity), FullInlinerNonExpressionSplitArgumentEvaluationOrder (low-severity), MissingSideEffectsOnSelectorAccess (low-severity) Solidity Compiler Bugs."

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ dotenvExpand.expand(dotenv.config())
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.20',
+    version: '0.8.26',
     settings: { optimizer: { enabled: true, runs: 200 } }
   },
   namedAccounts: {
